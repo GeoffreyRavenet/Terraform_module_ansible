a7a444c7281b7227042deacd7d1c7dab2eff204b
geoffrey ravenet <ravenet.geoffreypro@gmail.com>
2021-09-09 12:09:26 +0200

    clean de outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
 ###############################################################
 #                   STOCKAGE DE MES VARIABLE                  #
 ###############################################################
-
-output "ip_address_i2" {
-    value = aws_instance.ec2_myinstance_${var.NAME}.public_ip
-}
